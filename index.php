da39004edea880775ec74bfb71a9c20ff53e21a5
James Singleton <james@james.(none)>
2012-05-31 22:57:25 +0100

    Migrated from website and updated

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+	// Pragmatic tube status XSLT v3
+
+	// Copyright James Singleton 2012 (unop.co.uk)
+	// Licensed under a Creative Commons Attribution 3.0 Unported License.
+	
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Pragmatic Tube Status</title>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<style type="text/css">
+body {color: #FFF; background: #333;}
+a {color: #FFF;}
+table {border-collapse:collapse; width:100%;}
+table, th, td {border: 2px solid white;}
+th {color: #EEE; background: #111;}
+.l1 {color: #FFF; background: #AE6118;}
+.l2 {color: #FFF; background: #E41F1F;}
+.l3 {color: #FFF; background: #009FE0;}
+.l4 {color: #FFF; background: #8F989E;}
+.l5 {color: #FFF; background: #000000;}
+.l6 {color: #FFF; background: #0450A1;}
+.l7 {color: #113B92; background: #F8D42D;}
+.l9 {color: #FFF; background: #00A575;}
+.l8 {color: #113B92; background: #E899A8;}
+.l11 {color: #FFF; background: #893267;}
+.l12 {color: #113B92; background: #70C3CE;}
+.l81 {color: #FFF; background: #00BBB4;}
+.l82 {color: #FFF; background: #F86C00;}
+</style>
+</head>
+<body>
+<?php
+	// Load the XML source
+	$xml = new DOMDocument;
+	$xml->load("http://cloud.tfl.gov.uk/TrackerNet/LineStatus");
+
+	// Load the XSLT
+	$xsl = new DOMDocument;
+	$xsl->load('main.xsl');
+
+	// Configure the transformer
+	$proc = new XSLTProcessor;
+	$proc->importStyleSheet($xsl); // attach the XSLT
+	
+	// Do the transform
+	echo $proc->transformToXML($xml);
+
+	// Write date to page
+	date_default_timezone_set('Europe/London');
+	echo date('l jS \of F Y h:i:s A');
+?> 
+</body>
+</html>
---
+++ main.xsl
@@ -0,0 +1,46 @@
+<!--
+
+Pragmatic tube status XSLT v3
+
+Copyright James Singleton 2012 (unop.co.uk)
+Licensed under a Creative Commons Attribution 3.0 Unported License.
+
+-->
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:lul="http://webservices.lul.co.uk/">
+<xsl:output method="html"/>
+<xsl:template match="/">
+<table>
+<tr>
+<th>Line</th>
+<th>Status</th>
+</tr>
+<xsl:for-each select="//lul:LineStatus">
+<tr>
+<td>
+<xsl:attribute name="class">l<xsl:value-of select="lul:Line/@ID"/></xsl:attribute>
+<xsl:value-of select="lul:Line/@Name"/>
+</td>
+<td>
+<xsl:choose>
+<xsl:when test="lul:Status/@Description = &quot;Good Service&quot;">
+<xsl:attribute name="class">l9</xsl:attribute>Bog Standard Service</xsl:when>
+<xsl:otherwise>
+<xsl:attribute name="class">l2</xsl:attribute>
+<a><xsl:attribute name="href">http://m.tfl.gov.uk/mt/www.tfl.gov.uk/tfl/livetravelnews/realtime/tube/default.html?un_jtt_v_message=<xsl:choose>
+<xsl:when test="lul:Line/@ID = &quot;8&quot;">hammersmithandcity</xsl:when>
+<xsl:when test="lul:Line/@ID = &quot;12&quot;">waterlooandcity</xsl:when>
+<xsl:otherwise>
+<xsl:value-of select="lul:Line/@Name"/>
+</xsl:otherwise>
+</xsl:choose>
+</xsl:attribute>
+<xsl:value-of select ="lul:Status/@Description"/>
+</a>
+</xsl:otherwise>
+</xsl:choose>
+</td>
+</tr>
+</xsl:for-each>
+</table>
+</xsl:template>
+</xsl:stylesheet>
